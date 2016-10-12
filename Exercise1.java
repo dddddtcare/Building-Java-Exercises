984af9a11e7805779efeaca5a6d4443e16de9a2f
dddddtcare <jaychou0801@gmail.com>
2016-10-11 17:28:54 -0700

    .
    .

---
+++ Exercise1.java
@@ -0,0 +1,26 @@
+ /* a method called averageVowels that takes an ArrayList of Strings as a parameter and returns the average
+number of vowel characters (a, e, i, o, u) in all Strings in the list. If your method is passed an empty ArrayList, it
+should return 0.0.
+*/
+
+
+
+
+ 
+
+ 
+public class Exercise1{
+
+ArrayList<Integer> averageVowels(ArrayList<String> arrayList){
+	ArrayList<Integer> result=new ArrayList<Integer>();
+	int a=0,e=0,i=0,o=0,u=0;
+
+
+	
+}
+public static void main(String[] args){
+	ArrayList arrayList=['I','think','you','are','not','good'];
+	System.out.println(arrayList);
+
+}
+}
